d70fdf39312b306f63b004a978238c53ab6d268d
Gaurav Pingale <gauravpingale4@gmail.com>
2021-08-18 18:25:53 +0530

    shop page,nav updated

--- ecp/shop/views.py
+++ ecp/shop/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
+from math import ceil
 # Create your views here.
 
 def index(request):
-    return render(request,'shop/index.html')
+    # products = Product.objects.all()
+    # n = len(products)
+    # slides = n//4 + ceil((n/4)-(n//4))
+    # # params = {'no_of_slides':slides,'range','product':products}
+    # all_prods = [[products,range(1,slides),slides],
+    #              [products,range(1,slides),slides]]
+
+    all_prods = []
+    catprods= Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        slides = n//4 + ceil((n/4)-(n//4))
+        all_prods.append([prod,range(1,slides),slides])
+
+
+    params = {"all_prods":all_prods}
+    return render(request,'shop/index.html',params)
 
 def about(request):
-    return HttpResponse("about us")
+    return render(request,'shop/about.html')
 
 def contact(request):
     return HttpResponse("Contact us")
